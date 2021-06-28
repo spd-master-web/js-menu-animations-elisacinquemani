5815b7964a9ab2b1c66107988e33db3c269a9f3e
elisacinquemani <elisa.cinquemani@ied.edu>
2021-06-28 17:30:29 +0200

    Finished first excercise

--- js/one.js
+++ js/one.js
@@ -3,10 +3,53 @@ document.addEventListener('DOMContentLoaded', () => {
   const close = document.querySelector('.close');
 
   hamburger.addEventListener('click', (e) => {
-    // animate here when open the menu
-  })
+    gsap.to(".hamburger", { 
+      opacity: 0,
+      duration: 3,
+      ease: "elastic" 
+    });
+
+    gsap.to("aside", {
+      x: 0,
+      duration: 1,
+    });
+
+    gsap.to("li", {
+      opacity: 1,
+      duration: 0
+    });
+
+    gsap.to(".close", {
+      x: 300,
+      y: 0,
+      duration: 1,
+      ease: "power2"
+    });
+  });
 
   close.addEventListener('click', () => {
-    // animate here when close the menu
-  })
-})
+
+    gsap.to("aside", {
+      x: -500,
+      duration: 1
+    });
+
+    gsap.to("li", {
+      opacity: 0,
+      duration: 0
+    });
+
+    gsap.to(".hamburger", { 
+      opacity: 1,
+      duration: 3,
+      ease: "elastic" 
+    });
+
+    gsap.to(".close", {
+      x: -20,
+      y: -220,
+      duration: 2,
+      ease: "power2"
+    });
+  });
+});
